3ea00b1f8a25fa323b1371883eece043a5a80406
Akibara <laanemmone@gmail.com>
2026-03-24 14:40:47 +0700

    feat: enhance getInterviews function with pagination and unique counts for companies and users

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -23,19 +23,73 @@ exports.getInterviews = async (req, res, next) => {
         }
     }
 
-    try {
-        const interviews = await query;
+    query = query.populate({
+        path: 'user',
+        select: 'name email'
+    });
+
+    const uniqueCompany = (await Interview.aggregate([
+        {
+            '$group': {
+                '_id': '$company',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$count': 'totalCompany'
+        }
+    ]))[0]?.totalCompany || 0;
+    const uniqueUser = (await Interview.aggregate([
+        {
+            '$group': {
+                '_id': '$user',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$count': 'totalUser'
+        }
+    ]))[0]?.totalUser || 0;
+    const limit = parseInt(req.query.limit,10) || 3;
+    const total = await query.clone().countDocuments();
+    const totalPages = Math.ceil(total/limit) || 1;
+    const page = (parseInt(req.query.page,10)>totalPages)?totalPages:parseInt(req.query.page,10)||1;
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+
+    query=query.skip(startIndex).limit(limit);
+
+    try{
+        const interview = await query;
+
+        const pagination ={};
+        if(endIndex<total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+
+        if (startIndex>0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+
         res.status(200).json({
-            success: true,
-            count: interviews.length,
-            data: interviews
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: "Cannot find Interview"
-        });
+            success:true,
+            uniqueCompany,
+            uniqueUser,
+            count:interview.length,
+            totalPages,
+            totalCount:total,
+            pagination,
+            data:interview});
+    }catch(err){
+        res.status(400).json({success:false});
     }
 };
 
